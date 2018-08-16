deb538ea570df28a5e3b8844572a238552277e7f
Charles  Ohene <ckumi7@gmail.com>
2018-08-16 18:08:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var recipes = {}
 
-var updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value })
 }
 
-var updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  return object;
+}
+
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
   return object;
 }
