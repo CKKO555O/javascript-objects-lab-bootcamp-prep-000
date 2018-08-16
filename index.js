a6856bcd75e00fdbb9fd09662ec1c01aa9653a80
Charles  Ohene <ckumi7@gmail.com>
2018-08-16 18:00:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {}
+
+var updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+var updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
